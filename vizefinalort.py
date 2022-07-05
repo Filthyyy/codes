26bf80a3f4ca58dbf2d788395c25162b022b5b65
Sabri <108678313+Filthyyy@users.noreply.github.com>
2022-07-05 12:36:17 +0300

    Update vizefinalort.py

--- vizefinalort.py
+++ vizefinalort.py
@@ -2,3 +2,7 @@ vize = input('Vize Notunuz : ')
 final = input('Final Notunuz : ')
 ortalama=(float(vize)*0.4)+(float(final)*0.6)
 print("Ortalama :{0} ".format(ortalama))
+if(int(ortalama)<40):
+    print("Kaldınız")
+elif(int(ortalama)>=40):
+    print("Gectiniz")
